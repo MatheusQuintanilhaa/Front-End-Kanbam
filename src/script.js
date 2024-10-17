92d7b3efb486cb4c2220cff4ec85f38264203be0
KarolyneC <karolrsc@gmail.com>
2024-10-17 16:41:21 -0300

    adiciona lógica de drag & drop

--- src/script.js
+++ src/script.js
@@ -8,3 +8,31 @@ function salvarDados(dados, chave) {
 function carregarDados(chave) {
   return JSON.parse(localStorage.getItem(chave)) || [];
 }
+
+
+// Drag and Drop adicionados 
+document.querySelectorAll('drag-list').forEach(card => {
+  card.addEventListener('dragstart', e => {
+    e.currentTarget.classList.add('dragging')
+  })
+
+  card.addEventListener('dragend', e => {
+    e.currentTarget.classList.remove('dragging');
+  })
+})
+
+document.querySelectorAll('drag-column').forEach(column => {
+  column.addEventListener('dragover', e => {
+    e.preventDefault();
+    e.currentTarget.classList.add('cards-hover');
+  })
+
+
+column.addEventListener('dragleave', e => {
+  e.currentTarget.classList.remove('cards-hover');
+})
+  column.addEventListener('drop', e => {
+    const dragCard = document.querySelector('.drag-list.dragging');
+    e.currentTarget.appendChild(dragCard);
+  })
+})
