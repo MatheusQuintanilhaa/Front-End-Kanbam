8fb9e3b5dea1038fb41db732788c47d709474e0d
MatheusQuintanilhaa <matheussantos.quintanilha@gmail.com>
2024-10-17 22:06:36 -0300

    Testando

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-
 // Armazenar os dados no localStorage
 function salvarDados(dados, chave) {
   localStorage.setItem(chave, JSON.stringify(dados));
@@ -9,30 +8,31 @@ function carregarDados(chave) {
   return JSON.parse(localStorage.getItem(chave)) || [];
 }
 
+// Drag and Drop adicionados
+document.querySelectorAll("drag-list").forEach((card) => {
+  card.addEventListener("dragstart", (e) => {
+    e.currentTarget.classList.add("dragging");
+  });
 
-// Drag and Drop adicionados 
-document.querySelectorAll('drag-list').forEach(card => {
-  card.addEventListener('dragstart', e => {
-    e.currentTarget.classList.add('dragging')
-  })
+  card.addEventListener("dragend", (e) => {
+    e.currentTarget.classList.remove("dragging");
+  });
+});
 
-  card.addEventListener('dragend', e => {
-    e.currentTarget.classList.remove('dragging');
-  })
-})
-
-document.querySelectorAll('drag-column').forEach(column => {
-  column.addEventListener('dragover', e => {
+document.querySelectorAll("drag-column").forEach((column) => {
+  column.addEventListener("dragover", (e) => {
     e.preventDefault();
-    e.currentTarget.classList.add('cards-hover');
-  })
+    e.currentTarget.classList.add("cards-hover");
+  });
+
+  // teste
+  column.addEventListener("dragleave", (e) => {
+    e.currentTarget.classList.remove("cards-hover");
+  });
+  column.addEventListener("drop", (e) => {
+    const dragCard = document.querySelector(".drag-list.dragging");
+    e.currentTarget.appendChild(dragCard);
+  });
+});
 
 // teste
-column.addEventListener('dragleave', e => {
-  e.currentTarget.classList.remove('cards-hover');
-})
-  column.addEventListener('drop', e => {
-    const dragCard = document.querySelector('.drag-list.dragging');
-    e.currentTarget.appendChild(dragCard);
-  })
-})
